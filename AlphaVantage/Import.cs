c4b0eff4a376fc92dd0be2a5899c5cf284a425f0
rpriisholm <rpriisholm@hotmail.com>
2017-10-05 17:53:43 +0200

    Add project files.

---
+++ AlphaVantage/Import.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace AlphaVantage
+{
+    public class Import
+    {
+        public static void Program()
+        {
+            const string function = "TIME_SERIES_DAILY_ADJUSTED";
+            const string symbol = "BTC"; //"AMZN";//"MSFT";
+            const string apiKey = "11EJMYL88Q4DW5BT";
+
+            string json;
+
+            using (var web = new WebClient())
+            {
+                var url = $"https://www.alphavantage.co/query?function={function}&symbol={symbol}&outputsize=full&apikey={apiKey}";
+                json = web.DownloadString(url);
+                /* TEST */string result = json;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApiTester
+{
+
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            //GoogleFinance.Stocks.Program();
+            AlphaVantage.Import.Program();
+        }
+    }
+}
